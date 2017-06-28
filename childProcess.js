f8a9b3bf761c57fc8fe875c3abb360627f8a87ef
Joseph Brown <joe@sendlove.io>
2017-06-28 11:37:50 -0700

    Set inactivity to 30 min

--- childProcess.js
+++ childProcess.js
@@ -1,11 +1,11 @@
 var Livescore = require('./hltv-livescore');
 const args = process.argv[2];
 var CircularJSON = require('circular-json');
-var self = this; // 'this', the io client
+var self = this; // 'this', the child process
 self.time = 0;
 self.interval; // time remaining.
-var maxInactive = 600;
-var tick = 120; // remaining inactive alert
+var maxInactive = 1800; // TODO is this sufficient?
+var tick = 300; // remaining inactive alert
 
 const newGames = args.split(',').map(Number).filter(Boolean); // has to be INT array
 
@@ -19,16 +19,19 @@ var setInactivityTimer = function(time) {
     self.time = self.time - 1;
     _s = Number(self.time);
     if (_s % tick === 0 && _s > -1) {
-      console.log(newGames, 'inactive time remaining ', self.time);
+      // console.log(newGames, ' inactive time remaining ', self.time);
+      process.send(newGames + ' inactive time remaining ' + self.time);
     }
     if (_s <= 0) {
-      console.log(newGames, 'exiting due to inactivity');
+      // console.log(newGames, 'exiting due to inactivity');
+      process.send(newGames + ' exiting due to inactivity');
       throw new Error('exiting'); // child self-destructs
     }
   }, 1000);
 };
 
-console.log('In new child process, newGames = ', newGames);
+// console.log('Launching new child process, newGames = ', newGames);
+process.send('Launching new child process, newGames = ' + newGames);
 
 var live = new Livescore({
   gamesList: newGames
