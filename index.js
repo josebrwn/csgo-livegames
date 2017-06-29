9c2ae16ab8954672e2fb86ad112c769bf6848ac4
Joseph Brown <joe@sendlove.io>
2017-06-29 16:00:48 -0700

    writing to dummy API

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const livegames = require('./hltv-live-games');
 var logging = require('./logging');
 var Livescore = require('./hltv-livescore');
-var CircularJSON = require('circular-json');
 var cp = require('child_process');
+var request = require("request");
+var options = {
+    method: 'POST',
+    url: 'http://jsonplaceholder.typicode.com/posts',
+    headers: {
+        'cache-control': 'no-cache',
+        'content-type': 'application/json'
+    }
+};
+
+
 var newGames = [];
 var oldGames = [];
 var currentGames = [];
@@ -27,30 +37,61 @@ function scrapeMatchPage() {
 			oldGames = currentGames; // always reset oldGames
 			return;
 		}
-		// find any new listid's
 		newGames = leftDisjoin(currentGames, oldGames);
 		finishedGames = leftDisjoin(oldGames, currentGames);
 		console.log ('current games:', currentGames);
 		console.log('old games:', oldGames);
 		console.log('new games:', newGames);
 		console.log('finished games:', finishedGames);
+
+		/*
+			post newGames to the API
+		*/
+		var newGamesJSON = '{ "newGames": [' + newGames + '] }';
+		if (IsJsonString(newGamesJSON)) {
+			// post live games to API
+			// console.log(derp);
+			options.body = newGamesJSON;
+			request(options, function(error, response, body) {
+					if (error) throw new Error(error);
+					console.log(body); // use this to output the response from the API
+					// console.log(data);
+			});
+		}
+		else {
+			console.log('WARNING', newGamesJSON);
+		}
+
+		// TODO is a child process sometimes forked twice because of timing issues?
+
 		// send any new games to a newly spawned child process
 		if (newGames.length > 0) {
 			// fork the child process
 			var child = cp.fork('./childProcess.js', [newGames]);
 			// The only events you can receive from the child process are error, exit, disconnect, close, and message.
       child.on('message', function(data) {
-        try {
-          var dataStr = CircularJSON.stringify(data, null, 2);
-          console.log(data);
-        }
-        catch (err) {console.log(err);}
+
+				/*
+					post livescores to the API
+				*/
+				if (IsJsonString(data)) {
+					options.body = data;
+					request(options, function(error, response, body) {
+							if (error) throw new Error(error);
+							console.log(body); // use this to output the response from the API
+							// console.log(data);
+					});
+				}
+				else {
+					console.log('INFORMATION', data);
+				}
       });
 		}
 		oldGames = currentGames; // always reset oldGames
 	}); // getLiveGames
 } // scrapeMatchPage
 
+// main entry point: execute the scraper immediately, then every N seconds
 scrapeMatchPage();
 setInterval(scrapeMatchPage,loopEvery);
 
@@ -58,4 +99,14 @@ setInterval(scrapeMatchPage,loopEvery);
 function leftDisjoin(newArr, oldArr) {
   var oldSet = new Set(oldArr);
   return newArr.filter(function(x) { return !oldSet.has(x); });
+}
+
+// the API probably expects well-formed JSON
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
