2ded8db316321a8db2d9f15ae43629c11f4077c4
Joseph Brown <joe@sendlove.io>
2017-08-04 12:50:45 -0700

    fixed tweet length, added try/catch on post

--- config.js
+++ config.js
@@ -8,5 +8,6 @@ module.exports = {
   consumer_key: '***REMOVED***',
   consumer_secret: '***REMOVED***',
   access_token: '893460385809874944-XmrUzgSyVj5rKnExrvzHZu5O1e8MONn',
-  access_token_secret: 'YdTXt9GnGJR3ygqQjmPDXKJlryBLh0nAIxlCgVfUuK1Dl'
+  access_token_secret: 'YdTXt9GnGJR3ygqQjmPDXKJlryBLh0nAIxlCgVfUuK1Dl',
+  timeout_ms: 20000
 }
