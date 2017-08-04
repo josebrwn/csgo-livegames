7bec70173c1da6de0ba03db3d95e6fe8a35ce950
Joseph Brown <joe@sendlove.io>
2017-08-04 12:02:36 -0700

    Titter test on staging

---
+++ config.js
@@ -0,0 +1,12 @@
+//config.js
+/*
+  TWITTER APP CONFIGURATION
+  Owner	nxt335
+  Owner ID	893460385809874944
+*/
+module.exports = {
+  consumer_key: '***REMOVED***',
+  consumer_secret: '***REMOVED***',
+  access_token: '893460385809874944-XmrUzgSyVj5rKnExrvzHZu5O1e8MONn',
+  access_token_secret: 'YdTXt9GnGJR3ygqQjmPDXKJlryBLh0nAIxlCgVfUuK1Dl'
+}
