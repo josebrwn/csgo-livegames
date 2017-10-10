d5bc166963b4cb1ad8a8d2cdd7219a6fb50d3e08
Joseph Brown <joe@nxtake.com>
2017-10-10 15:47:36 -0700

    temporary fix for production tweets

--- tools.js
+++ tools.js
@@ -34,23 +34,38 @@ module.exports = {
     var T = new twit(config);
     var msg = JSON.parse(msg);
     var tweet = '';
-    if (parseFloat(msg["team1_win_percentage_live"]).toFixed(4) > parseFloat(msg["team2_win_percentage_live"]).toFixed(4))
-    {
-      tweet = tweet + msg["team1_name"] + " are a  " + parseFloat((msg["team1_win_percentage_live"])*100).toFixed(2) + "% favorite";
-      tweet = tweet + " over " +  msg["team2_name"] ;
-      tweet = tweet + ', score ' + msg["team1_score"] +" to "+ msg["team2_score"] ;
+    if (process.env.NODE_ENV === 'staging') {
+      if (parseFloat(msg["team1_win_percentage_live"]).toFixed(4) > parseFloat(msg["team2_win_percentage_live"]).toFixed(4))
+      {
+        tweet = tweet + msg["team1_name"] + " are a  " + parseFloat((msg["team1_win_percentage_live"])*100).toFixed(2) + "% favorite";
+        tweet = tweet + " over " +  msg["team2_name"] ;
+        tweet = tweet + ', score ' + msg["team1_score"] +" to "+ msg["team2_score"] ;
+      }
+      else
+      {
+        tweet = tweet + msg["team2_name"] + " are a " + parseFloat((msg["team2_win_percentage_live"])*100).toFixed(2) + "% favorite";
+        tweet = tweet + " over " +  msg["team1_name"] ;
+        tweet = tweet + ', score ' + msg["team2_score"] + " to  " + msg["team1_score"] ;
+      }
+      tweet = tweet + ", in match " +  msg["match_number"] + " of " +  msg["bestof"]  ;
+      tweet = tweet + '. http://***REMOVED***.com/matchups/' + msg["csgogame_id"];
     }
-    else
-    {
-      tweet = tweet + msg["team2_name"] + " are a " + parseFloat((msg["team2_win_percentage_live"])*100).toFixed(2) + "% favorite";
-      tweet = tweet + " over " +  msg["team1_name"] ;
-      tweet = tweet + ', score ' + msg["team2_score"] + " to  " + msg["team1_score"] ;
+    if (process.env.NODE_ENV === 'production') {
+      tweet = tweet + '\nt1 ' + msg["team1_id"];
+      tweet = tweet + '\nt2 ' + msg["team2_id"];
+      tweet = tweet + '\nt1wp ' + parseFloat(msg["team1_win_percentage"]).toFixed(4);
+      tweet = tweet + '\nt2wp ' + parseFloat(msg["team2_win_percentage"]).toFixed(4);
+      tweet = tweet + '\nround ' + msg["match_number"];
+      tweet = tweet + '\nmap ' + msg["map_name"];
+      tweet = tweet + '\nbo ' + msg["bestof"];
+      tweet = tweet + '\nt1wol ' + parseFloat(msg["team1_winodds_live"]).toFixed(4);
+      tweet = tweet + '\ns1 ' + msg["team1_score"];
+      tweet = tweet + '\ns2 ' + msg["team2_score"];
+      tweet = tweet + '\nt1wpl ' + parseFloat(msg["team1_win_percentage_live"]).toFixed(4);
+      tweet = tweet + '\nt2wpl ' + parseFloat(msg["team2_win_percentage_live"]).toFixed(4);
+      tweet = tweet + '\nt1w ' + msg["team1_wins"];
+      tweet = tweet + '\nt2w ' + msg["team2_wins"];
     }
-    tweet = tweet + ", in match " +  msg["match_number"] + " of " +  msg["bestof"]  ;
-    // tweet = tweet + '. http://***REMOVED***.com/matchups/' + msg["csgogame_id"];
-    tweet = tweet + '. http://***REMOVED***.com';
-
-
     tweet = tweet.substring(0,139);
 
     try {
