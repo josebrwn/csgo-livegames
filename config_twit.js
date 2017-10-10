7c7bd1f1435e55a375aa4e65e92a3523c344bab8
Joseph Brown <joe@nxtake.com>
2017-10-10 15:20:11 -0700

    new production twitter account

--- config_twit.js
+++ config_twit.js
@@ -3,11 +3,24 @@
   TWITTER APP CONFIGURATION
   Owner	nxt335
   Owner ID	893460385809874944
+  Owner ntake_odds
+  Owner ID 917869675429556225
 */
-module.exports = {
-  consumer_key: '***REMOVED***',
-  consumer_secret: '***REMOVED***',
-  access_token: '***REMOVED***',
-  access_token_secret: '***REMOVED***',
-  timeout_ms: 30000
+if (process.env.NODE_ENV === 'staging') {
+  module.exports = {
+    consumer_key: '***REMOVED***',
+    consumer_secret: '***REMOVED***',
+    access_token: '***REMOVED***',
+    access_token_secret: '***REMOVED***',
+    timeout_ms: 30000
+  }
+}
+else if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    consumer_key: '***REMOVED***',
+    consumer_secret: '***REMOVED***',
+    access_token: '***REMOVED***',
+    access_token_secret: '***REMOVED***',
+    timeout_ms: 30000
+  }
 }
