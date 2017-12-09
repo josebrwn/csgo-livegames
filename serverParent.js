9f2143b9f88044b27290d58c29b1239374cc6aed
Joseph Brown <joe@nxtake.com>
2017-12-09 16:49:39 -0700

    disabling twitter

--- serverParent.js
+++ serverParent.js
@@ -196,9 +196,9 @@ function postStatusChange (jsonVal) {
         }
         else {
           console.log('ok to tweet', self.time);
-          if (tools.sendTweet(body)) {
-            setTwitterTimer(timers["TWEET_SEC"]);
-          };
+          // if (tools.sendTweet(body)) {
+          //   setTwitterTimer(timers["TWEET_SEC"]);
+          // };
         }
       }
     }
