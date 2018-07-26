2675e3b9f1c4d86d61f9055333cc87fae446fb77
Joseph Brown <josebrwn@gmail.com>
2018-07-26 03:25:42 -0700

    changed scraper loop to 60 seconds

--- timers.js
+++ timers.js
@@ -1,12 +1,14 @@
 /*
  LOOP_EVERY_MS loop_every_ms > 1000 * alert_every_loop * 2
+ controls how often the scraper runs. was 180000 in production.
+
  ALERT_EVERY_LOOP every Nth loop report on inactivity
  MAX_INACTIVE inactivity can go an hour during breaks
  TIMEOUT_MS api timeout hits occasionally at 10000?
  WAIT_MS wait ms only useful if varying loop durations
 */
 module.exports = {
-    "LOOP_EVERY_MS": 180000,
+    "LOOP_EVERY_MS": 60000,
     "TIMEOUT_MS": 30000,
     "MAX_INACTIVE": 7200,
     "ALERT_EVERY_LOOP": 60,
